f0e17efaa6452b835b83ff5eaccdfea5943ccd05
dmytrobilash <dmytro.bilash@nure.ua>
2022-12-06 00:33:00 +0200

    "Project started. activity_main.xml was finaled"

---
+++ app/src/main/java/com/hfad/karnaughmap_java/MainActivity.java
@@ -0,0 +1,31 @@
+package com.hfad.karnaughmap_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button switchToSecondActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        switchToSecondActivity = findViewById(R.id.Karnaugh_2x2);
+        switchToSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivities();
+            }
+        });
+    }
+
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, map2x2.class);
+        startActivity(switchActivityIntent);
+    }
+}
---
+++ app/src/main/java/com/hfad/karnaughmap_java/map2x2.java
@@ -0,0 +1,14 @@
+package com.hfad.karnaughmap_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class map2x2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map2x2);
+    }
+}
