7c3e864d2f042221912690b787a6a124f3006560
dmytrobilash <dmytro.bilash@nure.ua>
2023-02-08 13:43:55 +0200

    view model for result

--- app/src/main/java/com/hfad/karnaughmap_java/ViewModel/TwoVariableViewModel.java
+++ app/src/main/java/com/hfad/karnaughmap_java/ViewModel/TwoVariableViewModel.java
@@ -3,11 +3,24 @@ package com.hfad.karnaughmap_java.ViewModel;
 
 import android.util.Log;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.hfad.karnaughmap_java.solvers.TwoVariables;
+
+import java.util.Arrays;
 
 public class TwoVariableViewModel extends ViewModel {
     public TwoVariableViewModel(){
         Log.i("VM", "VM created");
+    }
+    public MutableLiveData<String> liveData = new MutableLiveData<>();
+
+    public void result(int[] val){
+
+        Log.v("BBB", Arrays.toString(val));
+        TwoVariables solver = new TwoVariables(val);
+        liveData.setValue(solver.solve());
     }
 
     @Override
