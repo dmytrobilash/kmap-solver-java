cd6ea714f9125a61804c8b517b7af1c126eaaee9
dmytrobilash <dmytro.bilash@nure.ua>
2023-03-21 17:31:52 +0200

    minor project changes

--- app/src/main/java/com/hfad/karnaughmap_java/views/drawing/DrawSchemeActivity.java
+++ app/src/main/java/com/hfad/karnaughmap_java/views/drawing/DrawSchemeActivity.java
@@ -6,11 +6,14 @@ import androidx.core.content.FileProvider;
 
 import android.annotation.SuppressLint;
 import android.app.PendingIntent;
+import android.content.ClipData;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +23,7 @@ import android.widget.TextView;
 
 import com.hfad.karnaughmap_java.R;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -50,28 +54,47 @@ public class DrawSchemeActivity extends AppCompatActivity {
             }
         };
         layout.addView(lineView);
-        lineView.setDrawingCacheEnabled(true);
+
+
         ImageView share = findViewById(R.id.share);
         share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.v("Click", "Click");
-                Bitmap bitmap = Bitmap.createBitmap(lineView.getDrawingCache());
-                File file  = new File(getCacheDir(), "image.jpeg");
-                new Thread(() -> {
-                    try (FileOutputStream fos = new FileOutputStream(file)) {
-                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }).start();
+                lineView.setDrawingCacheEnabled(true);
+                lineView.buildDrawingCache();
+
+
+
+                /*File file = new File(getCacheDir(), "image.jpeg");
+                try (FileOutputStream fos = new FileOutputStream(file)) {
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
                 Intent intent = new Intent();
                 intent.setAction(Intent.ACTION_SEND);
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
                 intent.setType("image/jpeg");
-                intent.putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(getApplicationContext(), "com.hfad.karnaughmap_java.fileprovider", file));
-                //PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, Intent.createChooser(intent, "Share via"), PendingIntent.FLAG_UPDATE_CURRENT);
-                Intent chosenIntent = Intent.createChooser(intent, "Via");
-                startActivity(chosenIntent);
+                intent.putExtra(Intent.EXTRA_STREAM, file);
+                startActivity(Intent.createChooser(intent, "Send Image"));
+
+                Bitmap bitmap = Bitmap.createBitmap(lineView.getDrawingCache());
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("image/png");
+
+// Use ClipData to add the image URI to the Intent
+                Uri imageUri = Uri.parse(MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "title", null));
+                ClipData clipData = ClipData.newUri(getContentResolver(), "image", imageUri);
+                intent.setClipData(clipData);
+                intent.putExtra(Intent.EXTRA_STREAM, imageUri);
+
+                startActivity(Intent.createChooser(intent, "Send Image"));*/
             }
         });
     }
